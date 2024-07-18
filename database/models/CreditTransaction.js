58837d96383dc07947b6e235063f4d689b9e0db7
emirace <emmanuelakwuba57@gmail.com>
2024-07-18 12:33:52 +0100

    update

--- database/models/CreditTransaction.js
+++ database/models/CreditTransaction.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const creditTransactionSchema = new mongoose.Schema({
-    amount: { type: Number },
+const creditTransactionSchema = new mongoose.Schema(
+  {
+    amount: { type: Number, required: true },
     data: {
-        providerId: { type: String },
-        providerUrl: { type: String },
-        currency: { type: String },
-        amountCurrency: { type: Number }
+      providerId: { type: String, required: true },
+      providerUrl: { type: String, required: true },
+      currency: { type: String, required: true },
+      amountCurrency: { type: Number, required: true },
     },
-    type: { type: String },
-    user: { type: mongoose.Schema.ObjectId, ref: 'User' },
-    state: { type: String },
-    updatedAt: { type: Date, default: Date.now },
-    createdAt: { type: Date, default: Date.now }
-});
+    type: { type: String, required: true },
+    user: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
+    state: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('CreditTransaction',  creditTransactionSchema);
+module.exports = mongoose.model("CreditTransaction", creditTransactionSchema);
